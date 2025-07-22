fffef64b400312b7b33852ed3cfdb55a51fc5044
tk <t.khamiam@gmail.com>
2025-07-22 22:14:22 +0700

    removed all project files to restart

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,8 +0,0 @@
-import NextAuth from "next-auth";
-import { authOptions } from "./auth";
-
-// Create the auth handler
-const handler = NextAuth(authOptions);
-
-// Export the GET and POST handlers
-export { handler as GET, handler as POST }; 
--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Configure dev indicators with proper options
-  devIndicators: {
-    position: 'bottom-right',
-  },
-};
-
-module.exports = nextConfig; 
